8a6adf996dc0f8e964e535cd1e398087cc6ce863
Thalles Campagnani <thallescampagnani@gmail.com>
2024-07-15 17:18:07 -0300

    Atualizando para portas tipo-c e limpando arquivos a cada execução do código

--- setupModbusLinks/setup_modbus_links.sh
+++ setupModbusLinks/setup_modbus_links.sh
@@ -1,16 +1,19 @@
-#!/bin/bash
+!/bin/bash
 
 create_symbolic_link() {
     local device_info=$(udevadm info "$1" | awk -F'/' '/usb1/{if (!found) {for(i=1;i<=NF;i++) if($i ~ /^usb1$/) {print $(i+1); found=1;break}}}')
-    if [ "$device_info" = "1-5" ]; then #Porta trazeira superior direita
+    if [ "$device_info" = "1-1" ]; then #Porta USB TIPO-C superior
         ln -sf "$1" /dev/modbus/spuChA
-    elif [ "$device_info" = "1-6" ]; then #Porta trazeira superior esquerda
+    elif [ "$device_info" = "1-2" ]; then #Porta USB TIPO-C inferior
         ln -sf "$1" /dev/modbus/spuChB
     fi
 }
 
 # Garantir que exista diretório /dev/modbus/
 mkdir -p /dev/modbus/
+
+# Limpar o conteúdo do diretório
+rm /dev/modbus/*
 
 # Iterar sobre os dispositivos /dev/ttyUSB*
 for device in /dev/ttyUSB*; do
